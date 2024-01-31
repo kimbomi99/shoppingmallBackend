769ce212e01149e52bdfbbaee55aac7c1f247c5b
kimbomi99 <a99150316@gmail.com>
2024-01-31 21:59:17 +0900

    backend test

---
+++ src/main/java/com/example/product/ProductController.java
@@ -0,0 +1,43 @@
+package com.example.product;
+
+import java.io.File;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+public class ProductController {
+	@Autowired
+	ProductDAO productDao;
+	@RequestMapping("/list")
+	public List<Map<String, Object>> list(){
+		return productDao.list();
+	}
+
+	@RequestMapping("/insert")
+	public void insert(@RequestParam Map<String, Object> map, @RequestParam(required = false)MultipartFile img, HttpServletRequest request) {
+		String filename="-";
+		if(img != null && !img.isEmpty()) {
+			filename=img.getOriginalFilename();
+			try {
+				ServletContext application = request.getSession().getServletContext();
+				String path=application.getRealPath("/static/images/");
+				img.transferTo(new File(path + filename));
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		map.put("filename", filename);
+		productDao.insert(map);
+	}
+
+
+}
