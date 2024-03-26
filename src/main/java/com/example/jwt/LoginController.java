c9fd3809f569d8f098c054656ac94c8992ed3c4c
kimbomi99 <a99150316@gmail.com>
2024-03-26 20:16:07 +0900

    Spring Security + jwt 2

--- src/main/java/com/example/jwt/LoginController.java
+++ src/main/java/com/example/jwt/LoginController.java
@@ -7,10 +7,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
@@ -49,5 +53,37 @@ public class LoginController {
 		log.info(jwt);
 
 		return new ResponseEntity<String>(jwt, HttpStatus.OK);
+
+
+
+	}
+	//토큰 해석
+	@GetMapping("/user/info")
+	public ResponseEntity<?> userInfo(@RequestHeader(name="Authorization") String header){
+		log.info("===== header =====");
+		log.info("Authorization : " + header);
+
+		//Authorization : Bearer ${jwt}
+		String jwt = header.replace(SecurityConstants.TOKEN_PREFIX, "");
+
+		byte[] signingKey= jwtProp.getSecretKey().getBytes();
+
+		//토큰 해석
+		Jws<Claims> parsedToken = Jwts.parser().verifyWith( Keys.hmacShaKeyFor(signingKey) )
+				.build().parseSignedClaims(jwt);
+
+		log.info("paresedToken : " + parsedToken);
+
+		//uid : user
+		String username = parsedToken.getPayload().get("uid").toString();
+		log.info("username : "+ username);
+
+		//rol : [ROLE_USER, ROLE_ADMIN]
+		Claims claims = parsedToken.getPayload();
+		Object roles = claims.get("rol");
+		log.info("roles : "+ roles);
+
+
+		return new ResponseEntity<String>(parsedToken.toString(), HttpStatus.OK);
 	}
 }
